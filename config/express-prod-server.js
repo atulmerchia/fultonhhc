375368b2cb434ace6052fdb39fb54edc8f6de11b
Atul Merchia <atulmerchia@gmail.com>
2019-10-31 21:21:18 -0400

    Fix express server port specification

--- config/express-prod-server.js
+++ config/express-prod-server.js
@@ -1,4 +1,4 @@
-const port = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 const express = require('express');
 const app = express();
 
@@ -8,4 +8,4 @@ app.get('/', (req, res) => {
     res.send('An alligator approaches!');
 });
 
-app.listen(port, () => console.log(`Server running on port ${port}!`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}!`));
