20e146534f30426dc0a0bb59a83a893d6da67a51
Atul Merchia <atulmerchia@gmail.com>
2019-11-19 21:25:29 -0500

    force rebuild

--- config/express-prod-server.js
+++ config/express-prod-server.js
@@ -5,5 +5,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.static('build'));
+app.get('*', (req,res) => res.sendFile(path.join(__dirname + '/build/index.html'))
+});
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}!`));
