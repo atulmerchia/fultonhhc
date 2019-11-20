83fe9a38ec1ad2289fae4581e50aad836968e74f
Atul Merchia <atulmerchia@gmail.com>
2019-11-19 21:30:05 -0500

    force rebuild

--- config/express-prod-server.js
+++ config/express-prod-server.js
@@ -2,6 +2,7 @@ console.log('Starting production server');
 
 const PORT = process.env.PORT || 3000;
 const express = require('express');
+const path = require('path');
 const app = express();
 
 app.use(express.static('build'));
